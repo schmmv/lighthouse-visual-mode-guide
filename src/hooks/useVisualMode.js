2d0f5a6ef0d99c60f07288bff2d9b7561e4ce9ed
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-31 17:03:12 -0600

    Copy useVisualMode from sheduler into file

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,15 +3,24 @@ const { useState } = require("react");
 ///  Replace this code with your version of useVisualMode
 
 const useVisualMode = function(initial) {
-  const [history, setHistory] = useState([initial]);
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode, replace) {
-  };
+  const transition = function(newMode, replace = false) {
+    setMode(newMode);
+    if (!replace) {
+      setHistory([...history, newMode]);
+    }
+  }
 
   const back = function() {
-  };
-
+    if (history.length > 1) {
+      const copyHistory = [...history];
+      copyHistory.pop();
+      setHistory(copyHistory);
+      setMode(copyHistory[copyHistory.length - 1]);
+    }
+  }
   // Don't forget this -> history is needed here
   return { mode, transition, back, history };
 };
